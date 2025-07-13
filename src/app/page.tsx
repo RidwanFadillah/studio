e279efb1ac255afbdee7867fb1e908f1dd76fe79
Ridwan Figma <ridwanfigma@gmail.com>
2025-07-13 16:11:49 +0000

    saya ingin merubah layout, coba deh bagian tambah transaksinya itu berad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,11 +23,11 @@ export default function Home() {
       <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
         <div className="flex flex-col gap-8 lg:col-span-2">
           <BalanceDisplay transactions={transactions} />
+          <TransactionForm onAddTransaction={addTransaction} />
           <TransactionList transactions={transactions} />
         </div>
 
         <div className="flex flex-col gap-8">
-          <TransactionForm onAddTransaction={addTransaction} />
           <DataManagement transactions={transactions} onClearData={clearTransactions} />
         </div>
       </div>
